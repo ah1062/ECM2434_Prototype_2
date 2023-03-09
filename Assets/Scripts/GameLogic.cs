2092040e1afeeb0ddeff6f21ab51113a759012a7
ah1062 <95500509+ah1062@users.noreply.github.com>
2023-03-09 14:02:18 +0000

    Finish Prototype

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,27 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-    public Dictionary<int, GameObject> suspectObjects;
-    public static string correctSuspect;
+    Player playerScript;
+    public Dictionary<string, GameObject> suspectObjects;
+    public Dictionary<string, string> suspectDescriptions;
+    private string[] clues;
+    private static string correctSuspect;
 
     private void Awake() {
+        playerScript = GameObject.Find("Player").GetComponent<Player>();
+        
         GameObject g;
-        suspectObjects = new Dictionary<int, GameObject>();
+        suspectObjects = new Dictionary<string, GameObject>();
+        suspectDescriptions = new Dictionary<string, string>();
 
         for (int i = 1; i <= 5; i++) {
             g = GameObject.Find(string.Format("Suspect Slot {0}", i.ToString()));
-            Debug.Log(g.name);
-            suspectObjects.Add(i, g);
+            suspectObjects.Add(string.Format("Suspect Slot {0}", i.ToString()), g);
         }
 
-        SetSuspectSprites("12345");
-        SetCorrectSuspect("4");
+        SetSuspectSprites("42315");
+        SetCorrectSuspect("1");
+        
+        string[] descriptions = new string[5]{"CHARACTER A", "CHARACTER B", "CHARACTER C", "CHARACTER D", "CHARACTER E"};
+        SetSuspectDescriptions(descriptions);
+
+        clues = new string[10] {"A","B","C","D","E","F","G","H","I","J"};
     }
     public string GetCorrectSuspect() {
         return correctSuspect;
+    }
+
+    public string[] GetClues() {
+        return clues;
     }
 
     public void SetSuspectSprites(string sprite_codes) {
@@ -30,7 +45,7 @@ public class GameLogic : MonoBehaviour
         Sprite s;
         
         for (int i = 1; i <= 5; i++) {
-            g = suspectObjects[i];
+            g = suspectObjects[string.Format("Suspect Slot {0}", i.ToString())];
             sr = g.GetComponent<SpriteRenderer>();
 
             s = Resources.Load<Sprite>(string.Format("character_sprite_{0}", sprite_codes[i-1]));
@@ -40,6 +55,16 @@ public class GameLogic : MonoBehaviour
             } else {
                 Debug.Log(string.Format("SPRITE CODE {0} NOT FOUND", i.ToString()));
             }
+        }
+    }
+
+    public void SetClueDescriptions(string[] descriptions) {
+        clues = descriptions;
+    }
+
+    public void SetSuspectDescriptions(string[] descriptions) {
+        for (int i = 1; i <= 5; i++) {
+            suspectDescriptions.Add(string.Format("Suspect Slot {0}", i.ToString()), descriptions[i-1]);
         }
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     private Camera _mainCamera;
+    private bool accusing = false;
+    public TMP_Text characterText;
+    public TMP_Text clueText;
+    private GameObject lineup;
+    private GameObject clueParent;
+
     GameLogic gl;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _mainCamera = Camera.main;
         gl = GameObject.Find("LogicObject").GetComponent<GameLogic>();
+        lineup = GameObject.Find("AccuseToggleBackground");
+        
+        clueParent = GameObject.Find("CluesParentBackground");
+        ToggleClues(false);
     }
 
     // Update is called once per frame
@@ -26,15 +39,66 @@ public class Player : MonoBehaviour
 
             if (hit.collider != null)
             {
-                // Handle the click here
-                Debug.Log($"Clicked on {hit.collider.gameObject.name}");
+                if (hit.collider.gameObject.name == "AccuseToggle") {
+                    GameObject g = hit.collider.gameObject;
+                    SpriteRenderer sr = g.GetComponent<SpriteRenderer>();
 
-                if (hit.collider.gameObject.name.Contains(gl.GetCorrectSuspect()))
-                {
-                    Debug.Log("CORRECT");
-                    Destroy(hit.collider.gameObject);
+                    if (!accusing) {
+                        accusing = true;
+                        sr.sprite = Resources.Load<Sprite>("accuse_sprite_engaged");
+                    }
+                    else {
+                        accusing = false;
+                        sr.sprite = Resources.Load<Sprite>("accuse_sprite");
+                    } 
+                        
+                }
+
+                if (hit.collider.gameObject.name == "PageToggle") {
+                    SpriteRenderer sr = hit.collider.gameObject.GetComponent<SpriteRenderer>();
+                    
+
+                    if (lineup.activeInHierarchy) {
+                        lineup.SetActive(false);
+                        sr.sprite = Resources.Load<Sprite>("lineup_button_sprite");
+                        ToggleClues(true);
+
+                    }
+                    else {
+                        lineup.SetActive(true);
+                        sr.sprite = Resources.Load<Sprite>("diary_button_sprite");
+                        ToggleClues(false);
+                    }
+                }
+
+                if (gl.GetCorrectSuspect() != null) {
+                    if (accusing & hit.collider.gameObject.name.Contains(gl.GetCorrectSuspect()))
+                    {
+                        Debug.Log("CORRECT");
+                        Destroy(hit.collider.gameObject);
+                    }
+                    else if (hit.collider.gameObject.name.Contains("Suspect Slot")) {
+                        if (gl.suspectDescriptions != null) {
+                            characterText.text = gl.suspectDescriptions[hit.collider.gameObject.name];
+                        }
+                    }
+                }
+
+                if (hit.collider.gameObject.name.Contains("ClueButton")) {
+                    if (gl.GetClues() != null) {
+                        string name = hit.collider.gameObject.name;
+                        string code = string.Format("{0}", name[name.Length-1]);
+                        clueText.text = gl.GetClues()[Convert.ToInt32(code)];
+                    }
                 }
             }
         }
+    }
+
+    public void ToggleClues(bool state) {
+        clueText.gameObject.SetActive(state);
+        characterText.gameObject.SetActive(!state);
+
+        clueParent.SetActive(state);
     }
 }
